b54bd68cce4ac2d20c5fd06ea924b1be1d46b70b
RockLD <lid18149@gmail.com>
2019-06-06 10:34:47 +0800

    generator

--- Observer/EventGenerator.php
+++ Observer/EventGenerator.php
@@ -5,11 +5,12 @@ namespace Observer;
 abstract class EventGenerator
 {
     private $observers = array();
-    //添加观察者
+    //添加 观察者
     function addObserver(Observer $observer)
     {
         $this->observers[] = $observer;
     }
+    //通知 观察者
     function notify()
     {
         foreach ($this->observers as $observer)
@@ -18,3 +19,4 @@ abstract class EventGenerator
         }
     }
 }
+
