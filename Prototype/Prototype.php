e874529448878543cfcdf9e59fa423c1cf6dba6e
RockLD <lid18149@gmail.com>
2019-06-07 15:14:24 +0800

    prototype

---
+++ Prototype/Prototype.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * 原型模式
+ * 1、与工厂模式作用类似，都是用来创建对象
+ * 2、与工厂模式实现不同，原型模式是先创建好一个原型对象，然后通过clone原型对象来创建新的对象。这样就免去了类创建时重复的初始化操作
+ * 3、原型模式适用于大对象的创建。创建一个大对象需要很大的开销，如果每次new就会消耗很大，原型模式仅需内存拷贝即可
+ */
+
+ //假设是一个大对象，需要大量的消耗性计算
+class Prototype
+{
+    function init()
+    {
+
+    }
+
+    function init2()
+    {
+
+    }
+}
+
+$prototype = new Prototype();
+$prototype->init();
+$prototype->init2();
+//以上为大对象的初始化工作
+//可以直接克隆大对象，避免多次创建和繁琐的初始化工作
+$p1 = clone $prototype;
+$pe = clone $prototype;
